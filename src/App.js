9b1891c114c577f42446187f20b8257ffd9376c5
Murugeswari29 <murugeswari.chellapandi.er@gmail.com>
2023-12-29 15:55:22 +0530

    Created NoteZ React app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>NoteZ</h3>
+        <p>
+         Coming Soon...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
